98625caefcf7d8baf996fbeb0e05ff07afddc021
Hongyuan Qiu <hongyuan@MacBook-Pro.local>
2023-03-06 16:40:30 -0330

    Initial posts setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 export default function App() {
+  const posts = [];
+
   return (
-    <h1>Microblog</h1>
+    <>
+      <h1>Microblog</h1>
+      {posts.length === 0 ?
+        <p>There are no blog posts.</p>
+      :
+        posts.map(post => {
+          return (
+            <p key={post.id}>
+              <b>{post.author.username}</b> &mdash; {post.timestamp}
+              <br />
+              {post.text}
+            </p>
+          );
+        })
+      }
+    </>
   );
 }
