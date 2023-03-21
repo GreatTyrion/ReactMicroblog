a18f843b16872b36a90226524c8e2e092912e793
Hongyuan Qiu <hongyuan@MacBook-Pro.local>
2023-03-20 23:22:40 -0230

    Added components Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+
 export default function App() {
-  const posts = [];
+  const posts = [
+    {
+      id: 1,
+      text: 'Hello, world!',
+      timestamp: 'a minute ago',
+      author: {
+        username: 'susan',
+      },
+    },
+    {
+      id: 2,
+      text: 'Second post',
+      timestamp: 'an hour ago',
+      author: {
+        username: 'john',
+      },
+    },
+  ];
 
   return (
-    <>
-      <h1>Microblog</h1>
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
-    </>
+    <Container fluid className='App'>
+      <Header />
+      <Container>
+        {posts.length === 0 ?
+          <p>There are no blog posts.</p>
+        :
+          posts.map(post => {
+            return (
+              <p key={post.id}>
+                <b>{post.author.username}</b> &mdash; {post.timestamp}
+                <br />
+                {post.text}
+              </p>
+            );
+          })
+        }
+      </Container>
+    </Container>
   );
 }
